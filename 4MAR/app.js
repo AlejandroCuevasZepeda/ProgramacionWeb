3eeaf310aa153c8c74ac9256d89085f550b54c7e
20150853 CUEVAS ZEPEDA ALEJANDRO <20150853@TELEMATICA.UCOL.MX>
2020-03-04 08:48:13 -0600

    http

---
+++ 4MAR/app.js
@@ -0,0 +1,19 @@
+setTimeout(() => {console.log('Hola')}, 4000);
+setTimeout(() => {console.log('Hola 2')}, 0);
+console.log('Hola 3');
+
+/* Preguntas de Empresa:
+Housting
+Event Loop
+true, false
+
+node es asincrono
+js es sincrono 
+
+callback es una funcion llamada commo parametro en otra funcion
+
+Buscamos informacion de request http
+
+inmutable es que no puede cambiar
+
+Body Parser: https://www.npmjs.com/package/body-parser*/
